b864151952c5405d00756fdf7a569c463d867003
thiagoboth <thiagoboth@hotmail.com>
2022-02-19 14:13:34 -0300

    concluindo desafio principal com todos arquivos alterados

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,39 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+
+    setTasks(oldState => [...oldState, data])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    /*
+      Updated tasks faz uma copia de tasks, e como cada item de tasks tem propriedades simples,
+      ele faz a cópia dessas propriedades sem problemas, se fossem objetos dentro de objetos ou similares,
+      teria que fazer mais maps para que fossem pegos apenas as propriedades simples.
+      O foundItem já altera a updatedTaks, por justamente já estar pegando sua alocação na memória, que é o que
+      aconteceria com tasks se não usassemos o map.
+    */
+    const updatedTasks = tasks.map(task => ({ ...task }));
+
+    const foundItem = updatedTasks.find(item => item.id === id);
+
+    if(!foundItem)
+      return;
+
+    foundItem.done = !foundItem.done;
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks(oldState => oldState.filter(
+      task => task.id !== id
+    ));
   }
 
   return (
